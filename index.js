1e900220d2afa87c4f6859ea44b8515aee4afc20
temple7447 <62543994+temple7447@users.noreply.github.com>
2021-12-11 22:47:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// const isEmpty = !cart.line_items?.length;
+console.log('hello world');
+console.log("hello world you are welcome to website");
+let a=[1,3,4,5,6,7,8,6];
+console.log(a[2]+a[0]);
+let ability="snnlsbnsjlbvjdsvk";
+console.log(ability.length);
+
+
+
